36e0bac5eb9f66b02a5ad7be014ad0aaaf8abde3
SAOUD <aimenesaoud@gmail.com>
2025-08-28 22:44:53 +0200

    responsive de la sidebar

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
 console.log("Nickel👍");
 
+const hamburger = document.querySelector('.hamburger-menu');
 const sidebar = document.querySelector('.sidebar');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-sidebar.addEventListener('click', () =>{
+
+hamburger.addEventListener('click', () => {
     sidebar.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
 })
-
-
-
-
-const liensnav = document.querySelectorAll('.redirect-text');
-
-liensnav.forEach(lien => {
-    lien.addEventListener('mouseenter', function(ev) {
-        ev.currentTarget.style.color = '#ffffffff';
-    });
-    lien.addEventListener('mouseleave', function(ev) {
-        ev.currentTarget.style.color = '';
-    });
-});
